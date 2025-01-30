41872d44effd83f49a99312f5c90e801d5515df3
JP113z <josepablof113@gmail.com>
2025-01-30 17:03:51 -0600

    Ya tanto if como else se generan bien en codigo mips y funcional

--- asm.asm
+++ asm.asm
@@ -11,11 +11,11 @@ addi $sp, $sp, -8
 sw $ra, 4($sp)
 sw $fp, 0($sp)
 move $fp, $sp
-li $t0, 8
-li $t1, 6
+li $t0, 6
+li $t1, 8
 sgt $t2, $t0, $t1
 bne $t2, $zero, _etiqueta_true_3
-j _etiqueta_end_4
+j _etiqueta_end_if4
 _etiqueta_true_3:
 li $t0, 0x40B9999A
 mtc1 $t0, $f12
@@ -23,11 +23,21 @@ cvt.s.w $f0, $f0
 li $v0, 2
 mtc1 $t0, $f12
 syscall
-li $t6, 8
-move $t7, $t6
-_etiqueta_end_4:
-li $t8, 1
-move $v0, $t8
+li $t7, 8
+move $t8, $t7
+j _etiqueta_end_else5
+_etiqueta_end_if4:
+li $t0, 0x40F9999A
+mtc1 $t0, $f12
+cvt.s.w $f0, $f0
+li $v0, 2
+mtc1 $t0, $f12
+syscall
+li $s0, 10
+move $t8, $s0
+_etiqueta_end_else5:
+li $s1, 1
+move $v0, $s1
 jr $ra
 
 # Fin del main (_verano_)
